ea4439f21b779a8a81925cbcc6f19377db900a4c
一页书 <96804858+pili-book@users.noreply.github.com>
2024-07-15 18:54:46 +0800

    Add files via upload

---
+++ domains/xiao.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'NAS.',
+  domain: 'is-an.app',
+  subdomain: 'xiao',
+  owner: {
+    email: '397197703@qq.com',
+  },
+  record: {
+    TXT: ['dnspodcheck', '166aaa0abbc93d4a472e0ab2376a58c6'],
+    NS: ['f1g1ns1.dnspod.net', 'f1g1ns2.dnspod.net'],
+  },
+  proxy: false,
+})
